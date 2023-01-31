1d61ff23a38368fbc802ed0d8ee045fc83ca9777
yisus82 <djalma8@gmail.com>
2023-01-31 10:59:55 +0100

    Create the ball

---
+++ breakout/lib/components/ball.dart
@@ -0,0 +1,22 @@
+import 'package:flame_forge2d/flame_forge2d.dart';
+
+import '../forge2d_game_world.dart';
+
+class Ball extends BodyComponent<Forge2dGameWorld> {
+  final Vector2 position;
+  final double radius;
+
+  Ball({required this.position, required this.radius});
+
+  @override
+  Body createBody() {
+    final bodyDef = BodyDef()
+      ..type = BodyType.dynamic
+      ..position = position;
+    final ball = world.createBody(bodyDef);
+    final shape = CircleShape()..radius = radius;
+    final fixtureDef = FixtureDef(shape);
+    ball.createFixture(fixtureDef);
+    return ball;
+  }
+}
--- breakout/lib/forge2d_game_world.dart
+++ breakout/lib/forge2d_game_world.dart
@@ -1,8 +1,18 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 
+import 'components/ball.dart';
+
 class Forge2dGameWorld extends Forge2DGame {
   @override
   Future<void> onLoad() async {
+    await _initializeGame();
+  }
 
+  Future<void> _initializeGame() async {
+    final ball = Ball(
+      radius: 1.0,
+      position: size / 2,
+    );
+    await add(ball);
   }
 }
