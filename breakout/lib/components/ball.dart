62da7cb198456237ca7662e552c76a28879a5b6a
yisus82 <djalma8@gmail.com>
2023-02-01 08:50:27 +0100

    Rendering the ball

--- breakout/lib/components/ball.dart
+++ breakout/lib/components/ball.dart
@@ -1,10 +1,23 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flame/extensions.dart';
 
 import '../forge2d_game_world.dart';
 
 class Ball extends BodyComponent<Forge2dGameWorld> {
   final Vector2 position;
   final double radius;
+
+  final _gradient = RadialGradient(
+    center: Alignment.topLeft,
+    colors: [
+      const HSLColor.fromAHSL(1.0, 0.0, 0.0, 1.0).toColor(),
+      const HSLColor.fromAHSL(1.0, 0.0, 0.0, 0.9).toColor(),
+      const HSLColor.fromAHSL(1.0, 0.0, 0.0, 0.4).toColor(),
+    ],
+    stops: const [0.0, 0.5, 1.0],
+    radius: 0.95,
+  );
 
   Ball({required this.position, required this.radius});
 
@@ -23,6 +36,18 @@ class Ball extends BodyComponent<Forge2dGameWorld> {
     ball.createFixture(fixtureDef);
 
     return ball;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    final circle = body.fixtures.first.shape as CircleShape;
+    final paint = Paint()
+      ..shader = _gradient.createShader(Rect.fromCircle(
+        center: circle.position.toOffset(),
+        radius: radius,
+      ))
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(circle.position.toOffset(), radius, paint);
   }
 
   void reset() {
